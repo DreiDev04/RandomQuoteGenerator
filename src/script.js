0dd115a5d341f72bad2331dcee161126136ce086
dreidev_04 <tacujan.andrei@gmail.com>
2023-07-11 20:07:02 +0800

    fixing problems

--- src/script.js
+++ src/script.js
@@ -1,19 +1,23 @@
-
-
-const setQuote = (theQuote) => { 
-   $("#text").text(theQuote);
-
+const setQuote = (theQuote) => {
+  $("#text").text(theQuote);
 };
 
 const setAuthor = (theAuthor) => {
-   $("#author").text("- " + theAuthor);
-
+  $("#author").text("- " + theAuthor);
 };
 
 const setBG_color = () => {
-  const arrColor = ["#331D2C", "#F1C93B", "#1D5B79", "#EF6262", "#45CFDD", "#A76F6F", "#FF9EAA"];
+  const arrColor = [
+    "#331D2C",
+    "#F1C93B",
+    "#1D5B79",
+    "#EF6262",
+    "#45CFDD",
+    "#A76F6F",
+    "#FF9EAA",
+  ];
   const randomInteger = Math.floor(Math.random() * arrColor.length);
-  
+
   $(".bg").css("background-color", arrColor[randomInteger]);
   $(".text").css("color", arrColor[randomInteger]);
   $(".button").css("background-color", arrColor[randomInteger]);
@@ -27,11 +31,10 @@ const getQuote = () => {
       setAuthor(data.author);
       setBG_color();
     });
-  
-  // console.log("color changes");
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   getQuote();
-  $("#new-quote").on('click', getQuote);
+
+  $("#new-quote").on("click", getQuote);
 });
